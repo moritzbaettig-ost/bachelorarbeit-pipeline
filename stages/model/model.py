da5924569352c813e6041f8ff3552996385ee4d4
Moritz Bättig <moritz.baettig@bluewin.ch>
2022-07-16 14:12:53 +0200

    Plugin Architecture for Model

--- stages/model/model.py
+++ stages/model/model.py
@@ -4,16 +4,32 @@ from stages import Stage
 from dtos import DTO, ExtractionModelDTO
 from alerting.alert import Alert
 import sys
+import os
+import importlib
+
+class ModelPluginInterface:
+    # Define functions here
+    pass
 
 class Model(Stage, IObservable):
     def __init__(self, successor: 'Stage'):
+        if len(os.listdir('./stages/model/plugins')) == 0:
+            sys.exit("No model plugin detected. Please place default model plugin in the model plugin directory.")
+        sys.path.append('./stages/model/plugins')
+        self.plugins = [
+            importlib.import_module(f.split('.')[0], '.').Plugin()
+            for f in next(os.walk('stages/model/plugins'))[2]
+        ]
         self._observers = []
         super().__init__(successor)
 
     def run(self, dto: DTO) -> None:
         if not isinstance(dto, ExtractionModelDTO):
             sys.exit("Model: ExtractionModelDTO required.")
-            # MODEL STAGE
+        # MODEL STAGE
+        for plugin in self.plugins:
+            # define plugin iteration here
+            pass
 
     def attach(self, observer: IObserver) -> None:
         self._observers.append(observer)
