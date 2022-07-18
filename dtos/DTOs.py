dbed15a4b9f52e6cc17c28a245f0594c6ec5688d
Moritz Bättig <moritz.baettig@bluewin.ch>
2022-07-18 13:27:46 +0200

    Documentation: DTOs.py

--- dtos/DTOs.py
+++ dtos/DTOs.py
@@ -5,26 +5,71 @@ from type import Type
 
 @dataclass
 class DTO:
+    """
+    An interface used to represent data that is transferred between two pipeline stages.
+    """
+
     pass
 
 
 @dataclass
 class AcquisitionFilterDTO(DTO):
+    """
+    The data transfer object to transport data between the acquisition and the filter stage.
+
+    Attributes
+    ----------
+    message : IDSHTTPMessage
+        The HTTP request that has been acquired and has to be analyzed.
+    """
+
     message: IDSHTTPMessage
 
 
 @dataclass
 class FilterTypingDTO(DTO):
+    """
+    The data transfer object to transport data between the filter and the typing stage.
+
+    Attributes
+    ----------
+    message : IDSHTTPMessage
+        The HTTP request that has been acquired and has to be analyzed.
+    """
+
     message: IDSHTTPMessage
 
 
 @dataclass
 class TypingExtractionDTO(DTO):
+    """
+    The data transfer object to transport data between the typing and the extraction stage.
+
+    Attributes
+    ----------
+    message : IDSHTTPMessage
+        The HTTP request that has been acquired and has to be analyzed.
+    type : Type
+        The type of the HTTP request that has to be analyzed.
+    """
+
     message: IDSHTTPMessage
     type: Type
+    
 
 @dataclass
 class ExtractionModelDTO(DTO):
+    """
+    The data transfer object to transport data between the extraction and the model stage.
+
+    Attributes
+    ----------
+    features : dict
+        The dictionary containing the extracted features for the Machine Learning model.
+    type : Type
+        The type of the HTTP request that has to be analyzed.
+    """
+
     features: dict
     type: Type
     
