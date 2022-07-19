aae91098b2477ea2040b8fce0b7461e23cdb52e7
christophlandolt <christoph.landolt@ost.ch>
2022-07-19 16:41:47 +0200

    Code dokumentation model stage

--- stages/model/model.py
+++ stages/model/model.py
@@ -10,17 +10,88 @@ import importlib
 
 
 class ModelPluginInterface:
-    """This Class defines the default methods which must be implemented in a Model Plugin"""
+    """
+    This Class defines the default methods which must be implemented in a Model Plugin
+
+    Methods
+    ----------
+    train_model(training_data, training_labels)
+        This method trains the ML-Model with the given input data
+    predict(predicting_data)
+        This method makes a prediction with the pretrained ML-Model for the given input vector
+    """
 
     def train_model(self, training_data: list, training_labels: list) -> None:
+        """
+        This method trains an ML-Model with the given input data
+
+        Parameters
+        ----------
+        training_data: list
+            Is a list of dict objects fom all available reference data
+        training_labels: list
+            Is a list of all labels corresponding to the training_data list
+        """
         pass
 
-    def predict(self, predicting_data) -> list:
+    def predict(self, predicting_data: list) -> list:
+        """
+        This method predicts if a feature vector refers to an attack or not
+
+        Parameters
+        ----------
+        predicting_data: list
+            Is the feature vector which needs to be predicted as attack or not
+
+        Returns
+        ----------
+        list
+            Returns a list of the pattern [attack (1)/no attack(0), probability of the attack]
+        """
         pass
 
 
 class Model(Stage, IObservable):
+    """
+    A class that represents the last stage of the ML-IDS pipeline.
+    This class gets the computed feature vector from the extraction stage and classifies the request as attack or not.
+
+    Attributes
+    ----------
+    successor: Stage
+        The stage that follows this stage in the pipeline.
+    plugins: list
+        Is a list of all available plugins from the type ModelPluginInterface
+    db_handler: Database
+        This variable contains the connection to the database handling class
+    mode: str
+        This string contains if the pipeline is started in training or test mode
+    _observers: list
+        List of observers which is used for the alerting
+
+    Methods
+    ----------
+    run(dto)
+        The method that gets called by the previous stage.
+    attach(observer)
+        Attaches an observer to the observable.
+    detach(observer)
+        Detaches an observer from the observable.
+    notify(alert)
+        Notify all observers about an alert.
+    """
     def __init__(self, successor: 'Stage', mode: str, db_handler: Database):
+        """
+        Parameters
+        ----------
+        successor: Stage
+            The stage that follows this stage in the pipeline.
+        mode: str
+            This string contains if the pipeline is started in training or test mode
+        db_handler: Database
+            This variable contains the connection to the database handling class
+        """
+
         self.successor = successor
         if len(os.listdir('./stages/model/plugins')) == 0:
             sys.exit("No model plugin detected. Please place default model plugin in the model plugin directory.")
@@ -33,11 +104,21 @@ class Model(Stage, IObservable):
         self.mode = mode
         self._observers = []
 
-        # Set the Fabric in the Plugin
+        # Set the Factory dict in the Plugin
         for plugin in self.plugins:
             plugin.set_model(self.db_handler)
 
     def run(self, dto: DTO) -> None:
+        """
+        This method is called when a new request is processed by the pipeline
+
+        Parameters
+        ----------
+        dto: DTO
+            The data transfer object to transport data between the extraction and the model stage.
+        """
+
+        # Check if the correct dto was passed to the model stage
         if not isinstance(dto, ExtractionModelDTO):
             sys.exit("Model: ExtractionModelDTO required.")
         # MODEL STAGE
@@ -55,16 +136,38 @@ class Model(Stage, IObservable):
                 # Create an Alert
                 alert = Alert(msg=f"Attack detected with accuracy({ml_model_result[1]})")
                 self.notify(alert)
-                return
-            # define plugin iteration here
             pass
 
     def attach(self, observer: IObserver) -> None:
+        """
+        Attaches an observer to the observable.
+
+        Parameters
+        ----------
+        observer : IObserver
+            The observer to attach.
+        """
         self._observers.append(observer)
 
     def detach(self, observer: IObserver) -> None:
+        """
+        Detaches an observer from the observable.
+
+        Parameters
+        ----------
+        observer : IObserver
+            The observer to detach.
+        """
         self._observers.remove(observer)
 
     def notify(self, alert: Alert) -> None:
+        """
+        Notify all observers about an alert.
+
+        Parameters
+        ----------
+        alert: Alert
+            The alert being the reason for the notification.
+        """
         for observer in self._observers:
             observer.update(self, alert)
