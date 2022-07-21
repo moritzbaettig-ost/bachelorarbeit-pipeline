596b1002acb0917ffb396ff945720b90928884a6
Moritz Bättig <moritz.baettig@bluewin.ch>
2022-07-21 10:46:37 +0200

    Created Pipeline Class

--- pipeline.py
+++ pipeline.py
@@ -10,41 +10,43 @@ import argparse
 from database import DatabaseHandler
 
 
-def init_pipeline():
-    print("Initializing Pipeline")
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--host', type=str, default=None)
-    parser.add_argument('--mode', choices=['train', 'test'], default='test')
-    parser.add_argument('--logging', action=argparse.BooleanOptionalAction)
-    args = parser.parse_args()
+class Pipeline:
+    def init_pipeline(self):
+        print("Initializing Pipeline")
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--host', type=str, default=None)
+        parser.add_argument('--mode', choices=['train', 'test'], default='test')
+        parser.add_argument('--logging', action=argparse.BooleanOptionalAction)
+        args = parser.parse_args()
 
-    if len(sys.argv) > 4 or len(sys.argv) < 3:
-        sys.exit("Illegal amount of arguments")
+        if len(sys.argv) > 4 or len(sys.argv) < 3:
+            sys.exit("Illegal amount of arguments")
 
-    print(f"Running pipeline with host {args.host} and mode {args.mode}")
-    if args.logging:
-        print("Logging activated")
+        print(f"Running pipeline with host {args.host} and mode {args.mode}")
+        if args.logging:
+            print("Logging activated")
 
-    alerting_observer = Alerting()
-    database_handler = DatabaseHandler()
+        alerting_observer = Alerting()
+        database_handler = DatabaseHandler()
 
-    # STAGE: Model
-    stage_model = Model(None, args.mode, database_handler)
-    stage_model.attach(alerting_observer)
-    # STAGE: Extraction
-    stage_extraction = Extraction(stage_model, args.mode, args.logging, database_handler)
-    # STAGE: Typing
-    stage_typing = Typing(stage_extraction)
-    stage_typing.attach(alerting_observer)
-    # STAGE: Filter
-    stage_filter = RequestFilter(stage_typing)
-    stage_filter.attach(alerting_observer)
-    # STAGE: Acquisition
-    stage_acquisition = Acquisition(stage_filter, args.host)
-    
-    # Start Pipeline
-    stage_acquisition.run(None)
+        # STAGE: Model
+        stage_model = Model(None, args.mode, database_handler)
+        stage_model.attach(alerting_observer)
+        # STAGE: Extraction
+        stage_extraction = Extraction(stage_model, args.mode, args.logging, database_handler)
+        # STAGE: Typing
+        stage_typing = Typing(stage_extraction)
+        stage_typing.attach(alerting_observer)
+        # STAGE: Filter
+        stage_filter = RequestFilter(stage_typing)
+        stage_filter.attach(alerting_observer)
+        # STAGE: Acquisition
+        stage_acquisition = Acquisition(stage_filter, args.host)
+        
+        # Start Pipeline
+        stage_acquisition.run(None)
 
 
 if __name__ == '__main__':
-    init_pipeline()
+    pipeline = Pipeline()
+    pipeline.init_pipeline()
