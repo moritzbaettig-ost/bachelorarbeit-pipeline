bc6548ce6bacdca3d3a7170037efa7014aef64b9
Moritz Bättig <moritz.baettig@bluewin.ch>
2022-08-02 17:54:28 +0200

    Completely new traffic generator for seq2seq

--- test/seq2seqDataset/seq2seqTraffic.py
+++ test/seq2seqDataset/seq2seqTraffic.py
@@ -1,8 +1,16 @@
+import pathlib
+import sys
+directory = pathlib.Path(__file__)
+sys.path.append(directory.parent.parent.parent.__str__())
+
 import os
 import re
 import requests
 import pipeline
 import threading
+import time
+import ZODB, ZODB.FileStorage, ZODB.DB
+from datetime import datetime
 
 HTTP_RE = re.compile(r"ST@RT.+?INFO\s+(.+?)\s+END", re.MULTILINE | re.DOTALL)
 
@@ -55,51 +63,68 @@ def _process_request(req):
         i=i+1
     return my_method, my_uri, my_header, dictRequest
 
+def send_data(p: pipeline.Pipeline):
+    time.sleep(5)
 
-script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
-rel_path_train = "vulnbank_train.txt"
-rel_path_anomaly = "vulnbank_anomaly.txt"
-path_normal_data = os.path.join(script_dir, rel_path_train)
-path_anomaly_data = os.path.join(script_dir, rel_path_anomaly)
+    script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+    rel_path_train = "test_normal.txt"
+    rel_path_anomaly = "test_anomaly.txt"
+    path_normal_data = os.path.join(script_dir, rel_path_train)
+    path_anomaly_data = os.path.join(script_dir, rel_path_anomaly)
 
-http_requests_normal = get_requests_from_file(path_normal_data)
-http_requests_anomaly = get_requests_from_file(path_anomaly_data)
+    http_requests_normal = get_requests_from_file(path_normal_data)
+    http_requests_anomaly = get_requests_from_file(path_anomaly_data)
 
-p = pipeline.Pipeline()
-threading.Thread(target=p.init_pipeline)
+    try:
+        print("Start sending requests")
+        i = 1
+        p.stage_extraction.label = 1
+        for req in http_requests_anomaly:
+            my_method, my_uri, my_header, dictRequest = _process_request(req)
+            if my_method == 'GET':
+                print(f"Anomaly Request {i}")
+                requests.get(url=my_uri, headers=my_header)
+                i = i + 1
+            elif my_method == 'POST':
+                print(f"Anomaly Request {i}")
+                requests.post(url=my_uri, headers=my_header, data=dictRequest['body'])
+                i = i + 1
+            elif my_method == 'PUT':
+                print(f"Anomaly Request {i}")
+                requests.put(url=my_uri, headers=my_header, data=dictRequest['body'])
+                i = i + 1
+        i = 1
+        p.stage_extraction.label = 0
+        for req in http_requests_normal:
+            my_method, my_uri, my_header, dictRequest = _process_request(req)
+            if my_method == 'GET':
+                print(f"Normal Request {i}")
+                requests.get(url=my_uri, headers=my_header)
+                i = i + 1
+            elif my_method == 'POST':
+                print(f"Normal Request {i}")
+                requests.post(url=my_uri, headers=my_header, data=dictRequest['body'])
+                i = i + 1
+            elif my_method == 'PUT':
+                print(f"Normal Request {i}")
+                requests.put(url=my_uri, headers=my_header, data=dictRequest['body'])
+                i = i + 1
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("Finally")
+        r = p.stage_typing.get_path_reliabilities()
+        print(r)
+        f = open("test/seq2seqDataset/path_reliabilities.txt", "a")
+        f.write(str(datetime.now()))
+        f.write(", ")
+        f.write(str(r))
+        f.write("\n")
+        f.close()
+        os._exit(0)
 
-try:
-    print("Start sending requests")
-    i = 1
-    for req in http_requests_anomaly:
-        my_method, my_uri, my_header, dictRequest = _process_request(req)
-        if my_method == 'GET':
-            r = requests.get(url=my_uri, headers=my_header)
-            print(f"Anomaly Request {i}")
-            i = i + 1
-        elif my_method == 'POST':
-            r = requests.post(url=my_uri, headers=my_header, data=dictRequest['body'])
-            print(f"Anomaly Request {i}")
-            i = i + 1
-        elif my_method == 'PUT':
-            r = requests.put(url=my_uri, headers=my_header, data=dictRequest['body'])
-            print(f"Anomaly Request {i}")
-            i = i + 1
-    i = 0
-    for req in http_requests_normal:
-        my_method, my_uri, my_header, dictRequest = _process_request(req)
-        if my_method == 'GET':
-            r = requests.get(url=my_uri, headers=my_header)
-            print(f"Normal Request {i}")
-            i = i + 1
-        elif my_method == 'POST':
-            r = requests.post(url=my_uri, headers=my_header, data=dictRequest['body'])
-            print(f"Normal Request {i}")
-            i = i + 1
-        elif my_method == 'PUT':
-            r = requests.put(url=my_uri, headers=my_header, data=dictRequest['body'])
-            print(f"Normal Request {i}")
-            i = i + 1
-finally:
-    print("Finally")
-    # TODO: Get all path reliabilities
+
+if __name__ == '__main__':
+    p = pipeline.Pipeline("146.136.47.202", "train", False)
+    t = threading.Thread(target=p.init_pipeline).start()
+    send_data(p)
